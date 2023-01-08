f7df69fd1f16a8036628048ddf3957a401d2f895
vishnupriya41 <112959797+vishnupriya41@users.noreply.github.com>
2023-01-08 19:35:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,16 @@ model_nb = joblib.load('Project')
 vect = joblib.load('vect.pkl')
 
 def main():
-  st.title('Phishing URL Prediction') #creates a title in web app
-  ip = st.text_input('Enter URL:') #creates a text box in web app
+  st.title('Heart_Attack_Risk_Predictor ') #creates a title in web app
+  ip = st.text_input('Enter INPUT:') #creates a text box in web app
   if st.button('Predict'):
     data=[ip]
     cv=vect.transform(data).toarray()
     prediction=model_nb.predict(cv)
     result=prediction[0]
-    if result=='Good':
-      st.success("GOOD URL")
+    if result=='1':
+      st.success("TRUE")
     else:
-      st.error("Phishing URL")
+      st.error("FALSE")
    
 main()  
